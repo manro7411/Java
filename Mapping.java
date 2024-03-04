1279f0fd74f256860999c13ed4b820fce390ec47
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-05 00:25:21 +0200

    update -> Mapping

--- Mapping.java
+++ Mapping.java
@@ -1,18 +1,20 @@
-import java.util.*;
-public class Mapping {
+import java.util.HashMap;
+class Mapping {
     public static void main(String[] args) {
         HashMap<String,Integer> examscore = new HashMap<String,Integer>();
         examscore.put("Math", 75); 
         examscore.put("Science", 90); 
         examscore.put("Social", 70); 
-
-        // examscore.clear();
-
         System.out.println(examscore.toString());
         System.out.println(examscore.get("Math"));
         examscore.remove("Science");
         System.out.println(examscore.toString());
         System.out.println(examscore.size());
+        System.out.println(examscore.containsValue(Integer.valueOf(100)));
+        examscore.forEach((subject,score) -> {
+            examscore.replace(subject, score - 10);
+        });
+        System.out.println(examscore.toString());
     }
     
 }
