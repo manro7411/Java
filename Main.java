5a0152e77bdefcefeedf7552c2cb28a2a2ad101f
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-05 12:11:17 +0200

    update -> Main file

--- Main.java
+++ Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee();
-        e1.setId("1");;
-        e1.setName("Ratchanon Traitiprat");
-        e1.setSalary(30000.0);
-        e1.display();
-        System.out.println(e1.getName());
+        // Employee() is constructor 
+        // Employee e1 = new Employee();
+        // e1.setId("1");;
+        // e1.setName("Ratchanon Traitiprat");
+        // e1.setSalary(30000.0);
+        // e1.display();
+        // System.out.println(e1.getName());
+        // Employee e2 = new Employee();
+        // e2.setId("2");
+        // e2.setName("Passapol Phukhang");
+        // e2.setSalary(40000.0);
+        // e2.display();
+        // constructor is a special method to setting the beginning variable
+        Employee e1 = new Employee("Ratchanon Traitiprat","1",30000.0);
         Employee e2 = new Employee();
-        e2.setId("2");
-        e2.setName("Passapol Phukhang");
-        e2.setSalary(40000.0);
-        e2.display();
-
+        e1.display();
     }
 }
