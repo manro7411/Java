bf3ca711a5d87d441af5fec48c787a05dbd8b7a5
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-06 14:58:35 +0200

    update -> Main file

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Programmer p1 = new Programmer("Ratchanon",30000.0);
-        Accounting ac = new Accounting("Ratchanon",30000.0);
+        // Programmer p1 = new Programmer("Ratchanon",30000.0);
+        // Accounting ac = new Accounting("Ratchanon",30000.0);
+        //----
+        // Programmer p1 = new Programmer();
+        // p1.skill("1","2","3");
+        // // p1.bonus();
+        // Programmer p2 = new Programmer();
+        // p2.skill("1","2");
+        // Programmer p3 = new Programmer();
+        // p3.skill("1","2","3","4");
+        Programmer e1 = new Programmer();
+        e1.bonus();
+        Accounting e2 = new Accounting();
+        e2.bonus();
+
+
     }
     
 }
