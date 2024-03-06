946dea488625a62bb733ff4c9c64672f107b19cc
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-06 14:09:33 +0200

    update -> Company file

---
+++ Company.java
@@ -0,0 +1,8 @@
+class Company {
+    static String name ="Ratchanon Traitiprat Company";
+    static String founder = "1995";
+    static void service(){
+        System.out.println(Company.name);
+    }
+    
+}
