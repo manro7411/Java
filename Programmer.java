e989f11a528f1be5a3942ae9fc55c9de69775c92
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-06 14:58:56 +0200

    update -> Programmer file

--- Programmer.java
+++ Programmer.java
@@ -1,8 +1,27 @@
 class Programmer extends Employee{
-    public String skill = "Java Programming";
-    public Programmer(String name,Double salary){
-        super(name,name, salary);
-        System.out.println("I'm programmar.Moreover,i've used it for 10 years");
+    //overloading method 
+    public void skill(){
+        System.out.println("Don't have any programmer skill");
+    } 
+    public void skill(String language){
+        System.out.println("Skills : "+language);
     }
+    public void skill(String num1,String num2){
+        System.out.println("Skill : "+num1+""+num2);
+    }
+    public void skill(String...language){
+        for (int i = 0; i < language.length; i++) {
+            System.out.println("Skills :"+language[i]);
+        }
+    }
+    //overridding -> inheritance methond 
+    public void bonus(){
+        System.out.println("Bonus = 20");
+    }
+    // public String skill = "Java Programming";
+    // public Programmer(String name,Double salary){
+    //     super(name,name, salary);
+    //     System.out.println("I'm programmar.Moreover,i've used it for 10 years");
+    // }
     
 }
