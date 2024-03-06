9eb56da2e6e052dc40bd39c563fc380c5c336e5a
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-06 14:10:14 +0200

    update -> Programmer file

---
+++ Programmer.java
@@ -0,0 +1,8 @@
+class Programmer extends Employee{
+    public String skill = "Java Programming";
+    public Programmer(String name,Double salary){
+        super(name,name, salary);
+        System.out.println("I'm programmar.Moreover,i've used it for 10 years");
+    }
+    
+}
