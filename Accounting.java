7418d634dcbafe354f24e244c8471ffd42cc8f87
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-06 14:09:16 +0200

    update -> accounting file

---
+++ Accounting.java
@@ -0,0 +1,19 @@
+// class Accounting extends Employee{
+//     public Accounting(String name,Double salary){
+//         super(name,name, salary);
+//         System.out.println("I'm accounting");
+
+//     }
+// }
+
+
+/**
+ * Accounting
+ */class Accounting extends Employee{
+    public Accounting(String name,Double salary){
+        super(name,name,salary);
+        System.out.println("Im accouting");
+    }
+
+    
+}
